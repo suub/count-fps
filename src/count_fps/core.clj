bfb1c2c6dbe2d402b84fc754ef6192509b4d8144
mnoelte <noelte@suub.uni-bremen.de>
2014-09-08 19:27:56 +0200

    initial commit

---
+++ src/count_fps/core.clj
@@ -0,0 +1,6 @@
+(ns count-fps.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/count_fps/core_test.clj
@@ -0,0 +1,7 @@
+(ns count-fps.core-test
+  (:use clojure.test
+        count-fps.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
